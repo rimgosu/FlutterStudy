49fe0f6c0c750769972af15fd85bd0acbbe6d3a0
rimgosu <newnyup@gmail.com>
2023-12-27 21:13:40 +0900

    Wed Dec 27 21:13:40     2023

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -1,19 +1,42 @@
-// Flutter
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+
+void main() {
+  runApp(
+    MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Welcome to Flutter'),
         ),
-        body: Center(
-          child: Text('Hello world'),
+        body: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              color: Colors.red,
+              width: 100,
+              height: 100,
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.all(8.0),
+            ),
+            Container(
+              color: Colors.green,
+              width: 100,
+              height: 100,
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.all(8.0),
+            ),
+            Container(
+              color: Colors.blue,
+              width: 100,
+              height: 100,
+              padding: const EdgeInsets.all(8.0),
+              margin: const EdgeInsets.all(8.0),
+            ),
+          ],
         ),
       ),
-    );
-  }
+    ),
+  );
 }
