0f97e2ac2d5bbbf2b09a329b5bbcf12bfcf055d9
rimgosu <newnyup@gmail.com>
2023-12-28 21:00:28 +0900

    2023-12-28 21:00:28

---
+++ blog_web_app/lib/screen/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar( // ➊ 앱바 위젯 추가
+        // ➋ 배경색 지정
+        backgroundColor: Colors.orange,
+        // ➌ 앱 타이틀 설정
+        title: Text('Code Factory'),
+        // ➍ 가운데 정렬
+        centerTitle: true,
+      ),
+      body: Text('Home Screen'),
+    );
+  }
+}
